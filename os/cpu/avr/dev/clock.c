4bc8ade1cd0d766fa61852406c67dfa293e4bc9f
Zhong Hongbo <hongbo.zhong@windriver.com>
2014-04-10 09:23:44 +0800

    analysis the clock tick

--- os/cpu/avr/dev/clock.c
+++ os/cpu/avr/dev/clock.c
@@ -1,4 +1,3 @@
-
 #include "sys/clock.h"
 #include "dev/clock-avr.h"
 #include "sys/etimer.h"
@@ -8,136 +7,125 @@
 
 static volatile clock_time_t count;
 
-/*---------------------------------------------------------------------------*/
-//SIGNAL(SIG_OUTPUT_COMPARE0)
 ISR(AVR_OUTPUT_COMPARE_INT)
 {
-  ++count;
-  if(etimer_pending()) {
-    etimer_request_poll();
-  }
+	++count;
+	if(etimer_pending()) {
+		etimer_request_poll();
+	}
 }
 
-/* External clock source does not work ? */
-#if 0
-void
-clock_init(void)
-{
-  cli ();
-  TIMSK &= ((unsigned char)~(1 << (TOIE0)));
-  TIMSK &= ((unsigned char)~(1 << (OCIE0)));
-  /* Disable TC0 interrupt */
-
-  /**
-   *  set Timer/Counter0 to be asynchronous
-   *  from the CPU clock with a second external
-   *  clock(32,768kHz)driving it
-   */
-  ASSR |= (1 << (AS0));
-  TCCR0 = _BV (CS02) | _BV (CS01) | _BV (WGM1);
-
-  TCNT0 = 0;
-  OCR0 = 128;
-
-  TIMSK |= (1 << (OCIE0));
-  TIMSK |= (1 << (TOIE0));
-  sei ();
-}
-#endif
-
-/**************************************************************************/
-/*!
-    Set up the clock for a 1 msec tick interval
-*/
-/**************************************************************************/
+/*
+ * Set up the clock for a 1 msec tick interval
+ */
 void clock_setup()
 {
-    // choose internal clock as source
-    ASSR    = 0;
+	/* choose internal clock as source */
+	ASSR    = 0;
 
-    // reset the timer 0 counter to zero
-    TCNT0   = 0;
+	/* reset the timer 0 counter to zero */
+	TCNT0   = 0;
 
-    // set the compare reg to 32. With a prescaler val of 256, this would
-    // give us a ~1 msec interrupt
-    OCR0A = 32;
+	/*
+	 * set the compare reg to 32. With a prescaler
+	 * val of 256, this would give us a ~1 msec interrupt
+	 */
 
-    // Set timer control register:
-    //  - prescale: 256
-    //  - counter reset via comparison register (WGM01)
-    TCCR0A = _BV(WGM01);
-    TCCR0B = _BV(CS02);
+	/*
+	 * #define F_CPU                   8000000UL
+	 * #define AVR_CONF_TMR0_PRESCALE  256
+	 * #define AVR_TCCR0B_CONF         _BV(CS02)
+	 * Ravenusb:
+	 * #define CLOCK_CONF_SECOND	   1000
+	 * Raven:
+	 * #define CLOCK_CONF_SECOND       125
+	 *
+	 * Ravenusb:
+	 * 每秒8000000Hz, 每秒的时钟滴答数是1000(每秒的中断数)
+	 * 分频之后的频率为: 8000000 / 256 = 31250 Hz
+	 * 每个时钟滴答的时间为 1000000us / 1000 = 1000 us = 1 ms 
+	 * 每个时钟滴答所需要的时钟周期数为8000000 / 256 / 1000 = 31
+	 *
+	 * Raven:
+	 * 然而经过时钟预分频之后, 预分频的值为8000000 / 256 = 31250 Hz
+	 * 每个时钟滴答的时间为: 1000000us / 125 = 8000us = 8 ms
+	 * 每个时钟滴答所需要的时钟周期数为: 8000000 / 256 / 125 = 250 周期
+	 *
+	 * F_CPU = AVR_CONF_TMR0_PRESCALE * CLOCK_CONF_SECOND * OCR2A, less 1 for CTC mode
+	 * OCR0A = F_CPU / AVR_CONF_TMR0_PRESCALE / CLOCK_CONF_SECOND - 1;
+	 */
+	OCR0A = 32;
 
-    // Clear interrupt flag register
-    TIFR0 = TIFR0;
+	/*
+	 * Set timer control register:
+	 *  - prescale: 256
+	 *  - counter reset via comparison register (WGM01)
+	 */
+	TCCR0A = _BV(WGM01);
+	TCCR0B = _BV(CS02);
 
-    // Raise interrupt when value in OCR0 is reached. Note that the
-    // counter value in TCNT0 is cleared automatically.
-   TIMSK0 = _BV (OCIE0A);
+	/* Clear interrupt flag register */
+	TIFR0 = TIFR0;
+
+	/*
+	 * Raise interrupt when value in OCR0 is reached. Note that the
+	 * counter value in TCNT0 is cleared automatically.
+	 */
+	TIMSK0 = _BV (OCIE0A);
 }
 
-/*---------------------------------------------------------------------------*/
-void
-clock_init(void)
+void clock_init(void)
 {
-  cli ();
+	cli ();
 
 
-  //OCRSetup();
-  clock_setup();
+	clock_setup();
 
-  /*
-   * Counts the number of ticks. Since clock_time_t is an unsigned
-   * 16 bit data type, time intervals of up to 524 seconds can be
-   * measured.
-   */
-  count = 0;
+	/*
+	 * Counts the number of ticks. Since clock_time_t is an unsigned
+	 * 16 bit data type, time intervals of up to 524 seconds can be
+	 * measured.
+	 */
+	count = 0;
 
-  sei ();
+	sei ();
 }
 
-/*---------------------------------------------------------------------------*/
-clock_time_t
-clock_time(void)
+clock_time_t clock_time(void)
 {
-  return count;
+	return count;
 }
-/*---------------------------------------------------------------------------*/
+
 /**
  * Delay the CPU for a multiple of TODO
  */
-void
-clock_delay(unsigned int i)
+void clock_delay(unsigned int i)
 {
-  for (; i > 0; i--) {		/* Needs fixing XXX */
-    unsigned j;
-    for (j = 50; j > 0; j--)
-      asm volatile("nop");
-  }
+	unsigned j;
+
+	for (; i > 0; i--) {		/* Needs fixing XXX */
+		for (j = 50; j > 0; j--)
+			asm volatile("nop");
+	}
 }
 
-/*---------------------------------------------------------------------------*/
-/**
+/*
  * Wait for a multiple of 1 / 125 sec = 0.008 ms.
- *
  */
-void
-clock_wait(clock_time_t i)
+void clock_wait(clock_time_t i)
 {
-  clock_time_t start;
+	clock_time_t start;
 
-  start = clock_time();
-  while(clock_time() - start < (clock_time_t)i);
+	start = clock_time();
+	while(clock_time() - start < (clock_time_t)i);
 }
-/*---------------------------------------------------------------------------*/
-void
-clock_set_seconds(unsigned long sec)
+
+void clock_set_seconds(unsigned long sec)
 {
     // TODO
 }
 
-unsigned long
-clock_seconds(void)
+unsigned long clock_seconds(void)
 {
-  return count / CLOCK_SECOND;
+	return count / CLOCK_SECOND;
 }
